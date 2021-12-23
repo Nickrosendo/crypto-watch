23988d7cc2f58ac24cbf37db528cdb575dcd9ef7
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2021-12-23 08:24:02 -0300

    fix: update ga script to use next script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
@@ -6,25 +7,26 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-6VSYMC6YQC"></script>
 
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-6VSYMC6YQC'); 
-          `,
-            }}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-6VSYMC6YQC"
+            strategy="afterInteractive"
           />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6VSYMC6YQC');
+        `}
+          </Script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
